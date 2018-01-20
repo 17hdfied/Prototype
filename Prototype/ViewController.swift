e5adc79e94bb2e5bb9966bb0090dca2adddc29e5
17hdfied <hardikwason@gmail.com>
2018-01-20 19:44:24 +0530

    Login UI design

--- Prototype/ViewController.swift
+++ Prototype/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var email_txt: UITextField!
+    @IBOutlet weak var pass_txt: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func login_btn(_ sender: Any) {
     }
-
+    
+    @IBAction func fb_btn(_ sender: Any) {
+    }
+    
 
 }
 
